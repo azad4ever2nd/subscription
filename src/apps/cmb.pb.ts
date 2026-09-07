b165edac5d3b7c19cac10384322b56db0afe7c0b
azad <azad4ever2nd@outlook.com>
2026-09-07 18:27:51 +0800

    260907 8

--- src/apps/cmb.pb.ts
+++ src/apps/cmb.pb.ts
@@ -264,7 +264,7 @@ export default defineGkdApp({
           activityIds:
             'cmb.pb.app.h5container.webviewcontainer.PBWebContainerActivity',
           anyMatches: [
-            '(View > Button[text="关闭"] +n TextView[text="同步到动态"][clickable=true][visibleToUser=true]',
+            '(View > Button[text="关闭"] +n TextView[text="同步到动态"][clickable=true][visibleToUser=true])',
             '(View > @TextView[clickable=false][visibleToUser=true] + TextView[text="同步到动态"])',
             '(WebView[text="讨论区"] >n View >  @TextView[clickable=true][visibleToUser=true] +  TextView[text="同步到动态"])',
           ],
