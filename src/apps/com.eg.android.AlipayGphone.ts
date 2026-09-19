fb70df19f20aa2d4c25806f7d70369bf91d517f8
azad <azad4ever2nd@outlook.com>
2026-09-19 12:08:54 +0800

    260919 4

--- src/apps/com.eg.android.AlipayGphone.ts
+++ src/apps/com.eg.android.AlipayGphone.ts
@@ -266,7 +266,7 @@ export default defineGkdApp({
     {
       key: 14,
       name: '任务完成，返回领奖',
-      desc: '260919，fastQuery=false,clickable=true，添加返回，',
+      desc: '260919，fastQuery=false,clickable=true，添加 闪购0.1，返回，',
       rules: [
         {
           action: 'clickCenter',
@@ -276,6 +276,8 @@ export default defineGkdApp({
             '(@View > [text="任务完成"] + [text="返回会场" || text="返回领奖>"])',
             '(@View[clickable=true] >n [text="任务完成"] + [text="返回会场" || text="返回领奖>"])',
             'View > View > @Image[text="关闭弹窗"][clickable=true][visibleToUser=true] + View TextView[text="去买入"]',
+			'TextView[text="刷新"][clickable=true][visibleToUser=true]',
+		    'TextView[text="0.1"] - TextView[text="¥"] < View +n @TextView[text="先囤后送"][clickable=true][visibleToUser=true] + TextView[text="过期自动退"]',
           ],
           resetMatch: 'match',
           activityIds: [
@@ -627,7 +629,7 @@ export default defineGkdApp({
     {
       key: 35,
       name: '添加神券到首页，X掉',
-      desc: '260918，fastquery=false,clickable=false,添加 超级吃货卡体验版，闪购签到，闪购小程序，添加到首页，添加 生活缴费',
+      desc: '260919，fastquery=false,clickable=false,添加 闪购0.1，超级吃货卡体验版，闪购签到，闪购小程序，添加到首页，添加 生活缴费',
       rules: [
         {
           resetMatch: 'match',
@@ -653,8 +655,11 @@ export default defineGkdApp({
             '([text^="完成"] <<n View + View > TextView[text="领取奖励"][clickable=false][visibleToUser=true])',
             '(TextView[text="开心收下"] <<n View + TextView[text.length=0][clickable=false][visibleToUser=true])',
             '@TextView[clickable=false][visibleToUser=true] - View > View > View >  View >  View > View >  TextView[text="0"] + TextView[text=".1"] + TextView[text="元升级"]',
+			'@TextView[clickable=false][visibleToUser=true] - View > View > View >  View > View > TextView[text="去使用"]',
             'View > @TextView - View >5 TextView[text="开心收下"]',
             'View > View > View + TextView[text="去签到"][clickable=false][visibleToUser=true]',
+			'TextView[text="刷新"][visibleToUser=true]',
+		    'TextView[text="0.1"] - TextView[text="¥"] < View +n @TextView[text="先囤后送"][clickable=false][visibleToUser=true] + TextView[text="过期自动退"]',
           ],
         },
       ],
